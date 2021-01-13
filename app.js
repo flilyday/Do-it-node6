7a85330b211623a9d59713e9ce4b01448d586625
friday <eunbiryung@gmail.com>
2021-01-13 12:54:19 +0900

    Local-Strategy정의:로그인 실패 시 회원 가입 페이지로 이동

--- app.js
+++ app.js
@@ -106,10 +106,55 @@ passport.use('local-login', new Strategy({
 		}
 		console.log('아이디와 비밀번호가 일치합니다.');
 		return done(null, user);
-	})
-}
-)
+	});
+}));
 
+passport.use('local-signup', new LocalStrategy({
+	usernameField : 'email',
+	passwordField : 'password',
+	passReqToCallback : true
+}, function(req, email, password, done) {
+	var paramName = req.body.name || req.query.name;
+	console.log('passport의 local-signup 호출됨 : ' + email + ', ' + password + ', ' + paramName);
+
+	var database = app.get('database');
+	database.UserModel.findOne({'email':email}, function(err, user){
+		if(err) {
+			console.log('에러 발생');
+			return done(err);
+		}
+
+		if (user) {
+			console.log('기존에 계정이 있습니다.');
+			return done(null, false, req.flash('signupMessage', '계정이 이미 있습니다.'));
+		} else {
+			var user = new database.UserModel({'email':email, 'password':password, 'name':paramName});
+			user.save(function(err) {
+				if(err) {
+					console.log('데이터베이스에 저장 시 에러');
+					return done(null, false, req.flash('signupMessage', '사용자 정보 저장 시 에러가 발생했습니다.'));
+				}
+				console.log('사용자 데이터 저장함');
+				return done(null, user);
+			});
+		}
+	});
+
+}));
+
+passport.serializeUser(function(user, done){
+	console.log('serializeUser 호출됨')
+	console.dir(user);
+
+	done(null, user);
+})
+
+passport.deserializeUser(function(user, done) {
+	console.log('deserializeUser 호출됨');
+	console.dir(user);
+
+	done(null, user);
+});
 
 
  
