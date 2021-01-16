f683ca24a46f230a2d1021ccf2ed4d0c61fae0d8
friday <eunbiryung@gmail.com>
2021-01-16 09:45:31 +0900

    passport 모듈화 하기

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var expressSession = require('express-session');
   
 
 // 모듈로 분리한 설정 파일 불러오기
-var config = require('./config');
+var config = require('./config/config');
 
 // 모듈로 분리한 데이터베이스 파일 불러오기
 var database = require('./database/database');
@@ -77,84 +77,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// Passport Strategy 설정
-var LocalStrategy = require('passport-local').Strategy;
-const { Strategy } = require('passport');
+var configPassport = require('./config/passport');
+configPassport(app, passport);
 
-passport.use('local-login', new Strategy({
-	usernameField : 'email',
-	passwordField : 'passowrd',
-	passReqToCallback : true
-}, function(req, email, password, done) {
-	console.log('passport의 local-login 호출됨 : ' + email + ', ' + passowrd);
-
-	var database = app.get('database');
-	database.UserModel.findOne({'email':email}, function(err, user){
-		if (err) {
-			console.log('에러 발생함');
-			return done(err);
-		}
-		if (!user) {
-			console.log('사용자 아이디가 일치하지 않습니다.');
-			return done(null, fasle, req.flash('loginMessage', '등록된 계정이 없습니다.'));
-		}
-
-		var authenticated = user.authenticate(password, user._doc.salt, user._doc.hashed_password);
-		if(!authenticated) {
-			console.log('비밀번호가 일치하지 않습니다.');
-			return done(null, false, req.flash('loginMessage', '비밀번호가 일치하지 않습니다.'));
-		}
-		console.log('아이디와 비밀번호가 일치합니다.');
-		return done(null, user);
-	});
-}));
-
-passport.use('local-signup', new LocalStrategy({
-	usernameField : 'email',
-	passwordField : 'password',
-	passReqToCallback : true
-}, function(req, email, password, done) {
-	var paramName = req.body.name || req.query.name;
-	console.log('passport의 local-signup 호출됨 : ' + email + ', ' + password + ', ' + paramName);
-
-	var database = app.get('database');
-	database.UserModel.findOne({'email':email}, function(err, user){
-		if(err) {
-			console.log('에러 발생');
-			return done(err);
-		}
-
-		if (user) {
-			console.log('기존에 계정이 있습니다.');
-			return done(null, false, req.flash('signupMessage', '계정이 이미 있습니다.'));
-		} else {
-			var user = new database.UserModel({'email':email, 'password':password, 'name':paramName});
-			user.save(function(err) {
-				if(err) {
-					console.log('데이터베이스에 저장 시 에러');
-					return done(null, false, req.flash('signupMessage', '사용자 정보 저장 시 에러가 발생했습니다.'));
-				}
-				console.log('사용자 데이터 저장함');
-				return done(null, user);
-			});
-		}
-	});
-
-}));
-
-passport.serializeUser(function(user, done){
-	console.log('serializeUser 호출됨')
-	console.dir(user);
-
-	done(null, user);
-})
-
-passport.deserializeUser(function(user, done) {
-	console.log('deserializeUser 호출됨');
-	console.dir(user);
-
-	done(null, user);
-});
+var userPassport = require('./routes/user_passport');
+userPassport(router, passport);
 
  
 //라우팅 정보를 읽어들여 라우팅 설정
